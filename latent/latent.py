f4a1265291b7eaec9950650463ad9893ad899bec
Amartya Mitra <amitr003@ucr.edu>
2024-02-13 14:12:21 -0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   latent/latent.py

--- latent/latent.py
+++ latent/latent.py
@@ -1,10 +1,11 @@
 # Import packages from lib.py
 import sys
 sys.path.append('.')
-from data import *
+import lib
+# from data import *
 
-# Number of samples
-n_samples = 2000
-feature_dict, seed, add_noise = init_config(n_samples)
+# # Number of samples
+# n_samples = 2000
+# feature_dict, seed, add_noise = init_config(n_samples)
 
-X, y = get_toy_data(n_samples, feature_dict, seed, add_noise)
+# X, y = get_toy_data(n_samples, feature_dict, seed, add_noise)
