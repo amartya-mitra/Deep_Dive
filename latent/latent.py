66a85201b6f1fff79db04ba28258be50e957d657
Amartya Mitra <amitr003@ucr.edu>
2024-02-13 15:33:37 -0800

    Changes to be committed: 	new file:   __pycache__/data.cpython-310.pyc

--- latent/latent.py
+++ latent/latent.py
@@ -3,12 +3,11 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '')))
 # sys.path.append('.')
-import libc
-# import data
+import lib, data
 
 
-# # Number of samples
-# n_samples = 2000
-# feature_dict, seed, add_noise = init_config(n_samples)
+# Number of samples
+n_samples = 2000
+feature_dict, seed, add_noise = init_config(n_samples)
 
-# X, y = get_toy_data(n_samples, feature_dict, seed, add_noise)
+X, y = get_toy_data(n_samples, feature_dict, seed, add_noise)
