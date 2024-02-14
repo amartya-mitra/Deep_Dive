61aaffef9238e3cb167a7029d46ef5360a78f7bd
Amartya Mitra <amitr003@ucr.edu>
2024-02-14 12:58:43 -0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   latent/train_latent.py

--- latent/train_latent.py
+++ latent/train_latent.py
@@ -37,10 +37,6 @@ input_dim = X.shape[1]
 X = X.to(torch.float32)
 use_gpu = True
 
-import matplotlib
-matplotlib.use('TkAgg')
-print(matplotlib.get_backend())
-
 # Creating the model instance
 model = BinaryClassifier(input_dim, n_layer, hidden_dim, activation_func)
 model = train_model(model, epochs, use_es, use_gpu, train_dict, X, y.float().view(-1, 1), seed)
